1b0ed128c6b061de5412b055edd81b0fca3bb189
jdkYang <940604949@qq.com>
2020-04-17 20:33:43 +0800

    配置类

---
+++ im_forward_route/src/main/java/cn/yangjian/im/route/config/AppConfiguration.java
@@ -0,0 +1,29 @@
+package cn.yangjian.im.route.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class AppConfiguration {
+
+    @Value("${app.zk.root}")
+    private String zkRoot;
+
+    @Value("${app.zk.addr}")
+    private String zkAddr;
+
+
+    @Value("${server.port}")
+    private int port;
+
+    @Value("${app.zk.connect.timeout}")
+    private int zkConnectTimeout;
+
+    @Value("${app.route.way}")
+    private String routeWay;
+
+    @Value("${app.route.way.consitenthash}")
+    private String consistentHashWay;
+}
