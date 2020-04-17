4effbe8eefee8643913f32128b6f3da6b8bed1a2
jdkYang <940604949@qq.com>
2020-04-17 19:59:43 +0800

    用户信息

---
+++ im_common/src/main/java/cn/yangjian/im/common/pojo/IMUserInfo.java
@@ -0,0 +1,12 @@
+package cn.yangjian.im.common.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class IMUserInfo {
+    private Long userId;
+    private String userName;
+
+}
